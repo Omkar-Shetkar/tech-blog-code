097da91f1aad47483939c110315951a846425e6d
Omkar Shetkar <omkar.shetkar@outlook.com>
2025-11-19 19:28:49 +0530

    Update header text to include subtitle for Moments application

--- aws/apps/moments/frontend/src/App.js
+++ aws/apps/moments/frontend/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Moments</h1>
+      <h1>Moments - Place for Memories</h1>
     </div>
   );
 }
