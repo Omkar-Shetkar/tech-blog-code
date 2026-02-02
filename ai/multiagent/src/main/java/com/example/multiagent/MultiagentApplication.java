90f46ba55fae89f509e23461526035f355a376b1
omkar-shetkar <omkar.shetkar@outlook.com>
2026-02-02 09:58:17 +0530

    SummarizationAgent added

--- ai/multiagent/src/main/java/com/example/multiagent/MultiagentApplication.java
+++ ai/multiagent/src/main/java/com/example/multiagent/MultiagentApplication.java
@@ -5,7 +5,9 @@ import com.embabel.agent.api.annotation.Action;
 import com.embabel.agent.api.annotation.Agent;
 import com.embabel.agent.api.common.OperationContext;
 import com.embabel.agent.api.invocation.AgentInvocation;
+import com.embabel.agent.api.invocation.SupervisorInvocation;
 import com.embabel.agent.core.AgentPlatform;
+import com.embabel.agent.domain.io.UserInput;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,13 +34,17 @@ class CustomerController {
     }
 
     @GetMapping("/api")
-    public String query(@RequestParam("query") String query) {
-        var agentInvocation = AgentInvocation.create(agentPlatform, String.class);
-        return agentInvocation.invoke(query);
+    public SummarisedQuery query(@RequestParam("query") String query) {
+        return SupervisorInvocation.on(agentPlatform)
+                .returning(SummarisedQuery.class)
+                .invoke(new UserInput(query));
     }
 }
 
-record CustomerQuery(String orderId, String query) {
+record TranslatedQuery(String query) {
+}
+
+record SummarisedQuery(String orderId, String query) {
 }
 
 @Agent(description = "This agent translates customer queries to English.")
@@ -46,17 +52,28 @@ class TranslationAgent {
 
     @Action
     @AchievesGoal(description = "Translate customer query to English")
-    public String translateToEnglish(String customerQuery, OperationContext operationContext) {
+    public TranslatedQuery translateToEnglish(UserInput userInput, OperationContext operationContext) {
         return operationContext.ai()
                 .withAutoLlm()
                 .createObject(String.format("""
                         Translate the following text to English: {%s}
                         Output only the translated text.
-                        """, customerQuery), String.class);
+                        """, userInput.getContent()), TranslatedQuery.class);
     }
 }
 
 @Agent(description = "This agent summarizes customer queries.")
 class SummarizationAgent {
+
+    @Action
+    @AchievesGoal(description = "Summarize translated customer query for order processing")
+    public SummarisedQuery summarisedQuery(TranslatedQuery translatedQuery, OperationContext operationContext) {
+        return operationContext.ai()
+                .withAutoLlm()
+                .createObject(String.format("""
+                        Summarize the following customer query into a concise summary suitable for order processing: {%s}
+                        """, translatedQuery.query()), SummarisedQuery.class);
+
+    }
 
 }
